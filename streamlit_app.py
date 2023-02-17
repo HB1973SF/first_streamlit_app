0768f07b699e76e38be021149d66e7aff8d04c0e
HB1973SF <125443641+HB1973SF@users.noreply.github.com>
2023-02-17 09:14:12 +0800

    Update streamlit_app.py
    variabalising the fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,12 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display Fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 #streamlit.text(fruityvice_response.json()) 
 #the above just writes the data to the screen
 
@@ -34,4 +37,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # output it on screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+
 
